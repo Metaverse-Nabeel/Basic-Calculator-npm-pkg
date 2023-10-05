2f63febb78bc6b9da6b8f3628a14518708198249
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-05 22:13:43 +0500

    Make simple calculator

---
+++ index.ts
@@ -0,0 +1,61 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const calculator = await inquirer.prompt([
+  {
+    type: "number",
+    name: "num1",
+    message: "Enter first number: ",
+  },
+  {
+    type: "number",
+    name: "num2",
+    message: "Enter second number: ",
+  },
+  {
+    type: "list",
+    name: "operator",
+    message: "Select the operation you need to perform:",
+    choices: ["Add", "Subtract", "Multiply", "Divide", "Modulus"],
+  },
+]);
+
+let answer: number;
+
+switch (calculator.operator) {
+  case "Add":
+    answer = calculator.num1 + calculator.num2;
+    console.log(
+      "Result : " + calculator.num1 + " + " + calculator.num2 + " = " + answer
+    );
+    break;
+  case "Subtract":
+    answer = calculator.num1 - calculator.num2;
+    console.log(
+      "Result : " + calculator.num1 + " - " + calculator.num2 + " = " + answer
+    );
+    break;
+  case "Multiply":
+    answer = calculator.num1 * calculator.num2;
+    console.log(
+      "Result : " + calculator.num1 + " * " + calculator.num2 + " = " + answer
+    );
+    break;
+  case "Divide":
+    answer = calculator.num1 / calculator.num2;
+    console.log(
+      "Result : " + calculator.num1 + " / " + calculator.num2 + " = " + answer
+    );
+    break;
+  case "Modulus":
+    answer = calculator.num1 % calculator.num2;
+    console.log(
+      "Result : " + calculator.num1 + " mod " + calculator.num2 + " = " + answer
+    );
+    break;
+
+  default:
+    console.log("Please Enter Correct Operator!");
+    break;
+}
